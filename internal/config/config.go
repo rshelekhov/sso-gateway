736e67e070ce4b75107e6a95b6d8d19afec432b2
Roman Shelekhov <shelekhoff@gmail.com>
2024-10-21 00:13:09 +0400

    add config and server settings

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,57 @@
 package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+const CONFIG_PATH = "CONFIG_PATH"
+
+func MustLoad() *ServerSettings {
+	configPath := fetchConfigPath()
+
+	if configPath == "" {
+		panic("config path is empty")
+	}
+
+	return MustLoadPath(configPath)
+}
+
+func MustLoadPath(configPath string) *ServerSettings {
+	cfg := ServerSettings{}
+
+	viper.SetConfigFile(configPath)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("error finding or reading config file: %s", err)
+	}
+
+	viper.AutomaticEnv()
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		log.Fatalf("error unmarshalling config file into struct: %s: ", err)
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var v string
+
+	flag.StringVar(&v, "config", "", "path to config file")
+	flag.Parse()
+
+	if v == "" {
+		v = os.Getenv(CONFIG_PATH)
+	}
+
+	return v
+}
--- internal/config/server_settings.go
+++ internal/config/server_settings.go
@@ -1,1 +1,28 @@
 package config
+
+import "time"
+
+type ServerSettings struct {
+	AppEnv     string             `mapstructure:"APP_ENV"`
+	HTTPServer HTTPServerSettings `mapstructure:",squash"`
+	Clients    ClientsSettings    `mapstructure:",squash"`
+}
+
+type HTTPServerSettings struct {
+	Address          string        `mapstructure:"HTTP_SERVER_ADDRESS"`
+	Timeout          time.Duration `mapstructure:"HTTP_SERVER_TIMEOUT" envDefault:"10s"`
+	IdleTimeout      time.Duration `mapstructure:"HTTP_SERVER_IDLE_TIMEOUT" envDefault:"60s"`
+	RequestLimitByIP int           `mapstructure:"HTTP_SERVER_REQUEST_LIMIT_BY_IP" envDefault:"100"`
+}
+
+type ClientsSettings struct {
+	SSO Client `mapstructure:",squash"`
+}
+
+type Client struct {
+	Address      string        `mapstructure:"SSO_CLIENT_ADDRESS"`
+	Timeout      time.Duration `mapstructure:"SSO_CLIENT_TIMEOUT"`
+	RetriesCount int           `mapstructure:"SSO_CLIENT_RETRIES_COUNT"`
+	// TODO: implement secure transport
+	// Insecure     bool          `mapstructure:"SSO_CLIENT_INSECURE"`
+}
